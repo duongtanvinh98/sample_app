2265204682b6bd44f1d2a0784a9e59c4dd9eb907
tan vinh <duongtanvinh1409@gmail.com>
2019-05-06 09:58:56 +0700

    Chapter 7: Sign up

--- Gemfile
+++ Gemfile
@@ -4,7 +4,9 @@ git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
 end
+gem "bcrypt", "3.1.12"
 gem "bootstrap-sass", "3.3.7"
+gem "config"
 gem "rails", "~> 5.1.7"
 # Use Puma as the app server
 gem "puma", "~> 3.7"
---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  before_save{email.downcase!}
+  validates :name, presence: true,
+    length: {maximum: Settings.app.user.name_length}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: Settings.app.user.email},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+  has_secure_password
+  validates :password, presence: true,
+    length: {minimum: Settings.app.user.password}
+end
